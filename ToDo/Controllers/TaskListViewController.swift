f0aba458928fe60b12ff7f0639e6327fc988d8f0
AkhilDeep Shrivastava <akhildeep.shrivastava@gmail.com>
2021-01-20 22:38:11 -0500

    Add Table View Delegate and DataSource

--- ToDo/Controllers/TaskListViewController.swift
+++ ToDo/Controllers/TaskListViewController.swift
@@ -17,16 +17,23 @@ class TaskListViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
+}
+
+extension TaskListViewController: UITableViewDataSource, UITableViewDelegate {
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskTableViewCell", for: indexPath)
+        
+        return cell
+    }
+    
 }
